5fa36a1c9dbbc49dab711149e65363e733acbb3f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-21 18:40:48 +0200

    values

--- app.js
+++ app.js
@@ -21,13 +21,15 @@ form.addEventListener('submit', (e) => {
   const value = parseInt(amount.value);
   const random = Math.floor(Math.random() * text.lenght);
 
-  if (isNaN(value) || value < 0 || value > 9) {
+  if (isNaN(value) || value <= 0 || value > 9) {
     result.innerHTML = `<p class="result">${text[random]}</p>`;
   }
   else {
     let tempText = text.slice(0, value);
     tempText = tempText.map(function (item) {
-      return `<p class="result">${text[random]}</p>`;
-    });
+      return `<p class="result">${item}</p>`;
+    })
+      .join("");
+    result.innerHTML = tempText;
   }
 });  
